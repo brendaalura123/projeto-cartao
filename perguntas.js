1ad9ef960647946ec6d2b432913a10534632eb48
brendaalura123 <168103914+brendaalura123@users.noreply.github.com>
2024-10-08 11:09:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'química organica',
+    ' qual a fórmula estrutural do acetileno ?',
+    'H-C-C-C-H'
+)
+
+criaCartao(
+    'química organica',
+    ' qual é o nome do composto CH3-CH2-CH2-CH2-CH3?',
+    ' pentano'
+)
+
+criaCartao(
+    'química organica',
+    'O que é um hidrocarboneto?',
+    ' Os hidrocarbonetos são um grupo de compostos orgânicos que possui apenas átomos dos elementos carbono e hidrogênio'
+
+)
+
+criaCartao(
+    'química organica',
+    'qual é a nomenclatura: CH2=CH-CH2-CH3?',
+    'but-1-eno'
+)
